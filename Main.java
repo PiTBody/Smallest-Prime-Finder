b43554f9e439ca75522dce4fcaf454d8d618ef37
PiTBody <mykhailo.papenko@gmail.com>
2026-02-24 22:44:55 -0800

    Fixed typo.

--- Main.java
+++ Main.java
@@ -26,7 +26,7 @@ void main() {
         } catch (IOException e) {
             LOGGER.log(Level.SEVERE, e, e::getMessage);
         }
-        IO.println("Finished one of the threads.");
+        IO.println("One of the threads finished.");
     }
 }
 
